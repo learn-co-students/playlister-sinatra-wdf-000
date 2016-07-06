f48a20ad8d96c945d9e84c7e02bb3eb67b9702b3
lastromanticx <giladbarkan@live.com>
2016-07-06 16:17:10 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < Sinatra::Base
-  enable :sessions
-  use Rack::Flash
-
   register Sinatra::ActiveRecordExtension
+
+  enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 

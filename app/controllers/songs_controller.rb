489edf354467f2758dc52c746b64f36fea9c93c1
ozPop <ozziepopovas@gmail.com>
2016-08-13 00:48:37 -0400

    add edit song view, update song controller, show view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,15 +22,42 @@ class SongsController < ApplicationController
     redirect "/songs/#{song.slug}"
   end
 
-  get "/songs/:slug" do
-    # binding.pry
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/edit'
+  end
+
+  post '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    # update artist if not empty
+    if params[:song][:artist] != ""
+      Artist.find(@song.artist_id).update(name: params[:song][:artist])
+    end
+    # update genres unless empty
+    unless params[:song][:genre_ids].empty?
+      @song.genres.clear
+      params[:song][:genre_ids].each do |id|
+        genre = Genre.find(id)
+        @song.genres << genre
+      end
+    end
+    @song.save
+    session[:success] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+  end
+
+  get '/songs/:slug' do
     @success = session[:success]
+    # binding.pry
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
 
-  def find_artist
-    Artist.all.detect {|a| a.name.downcase == params[:artist][:name].downcase }
+  helpers do
+    def find_artist
+      Artist.all.detect {|a| a.name.downcase == params[:artist][:name].downcase }
+    end
   end
+
 
 end

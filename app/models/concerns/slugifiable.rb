5abd93575dcac2afc385b11c3dd58633b8a8f164
ozPop <ozziepopovas@gmail.com>
2016-08-12 21:46:40 -0400

    refactor find_by_slug

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,27 +1,13 @@
 module Slugifiable
   module ClassMethods
     def find_by_slug(slug)
-      # this needs to be made to work with all
-        # songs, artists, genres
-      # currently coded to work with spec input
-      escape_words = ["with", "a", "the"]
-      # r = slug.gsub(/[-]/, ' ').split(" ").map {|w| w.capitalize}.join(" ")
-      r = slug.gsub(/[-]/, ' ').split(" ")
-      r.map! do |w|
-        if !escape_words.include?(w)
-          w.capitalize
-        else
-          w
-        end
-      end
-      r = r.join(" ")
-      self.all.find_by(name: r)
+      self.all.detect {|song| song.slug == slug}
     end
   end
 
   module InstanceMethods
     def slug
-      # self.name.gsub(/[ ]/, '-').downcase
+      # got regex from: http://slugify.net/libraries
       self.name.strip.downcase.gsub(/\W+/, '-')
     end
   end

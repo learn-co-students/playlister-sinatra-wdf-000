3f40e33934aaaaa461a360b39a77af08336bd76f
fidelscodes <fidelscodes@gmail.com>
2016-08-17 14:09:48 -0400

    Add show pages for artists and genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,11 @@ class GenresController < ApplicationController
     @genres = Genre.all
     erb :'/genres/index'
   end
+
+  get '/genres/:slug' do
+    @genre = Genre.find_by_slug(params[:slug])
+    @genre_artists = @genre.songs.collect {|s| s.artist}
+    @genre_songs = @genre.songs
+    erb :'/genres/show'
+  end
 end

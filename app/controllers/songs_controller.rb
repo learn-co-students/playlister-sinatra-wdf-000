1bac95a50232f9c64575011daf524d7c73e3495e
Wendy Xu <guangw.xu@gmail.com>
2016-08-31 18:32:57 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,60 @@
+require 'rack-flash'
+
+class SongsController < ApplicationController
+	
+	use Rack::Flash
+
+	get '/songs' do 
+		@songs = Song.all
+		erb :'/songs/index'
+	end
+
+	get '/songs/new' do 
+	 erb :'/songs/new'
+	end
+
+	get '/songs/:slug' do 
+		@song = Song.find_by_slug(params[:slug])
+		@artist = @song.artist
+		@genres = @song.genres
+		erb :'/songs/show'
+	end
+
+	post '/songs' do
+		@song = Song.create(params[:song])
+		@artist = Artist.find_or_create_by(params[:artist])
+		@song.artist = @artist
+		
+		params[:genres].each do |genre_id|
+			@song.genres << Genre.all.find{|i| i.id == genre_id.to_i }
+		end
+
+		@song.save
+
+		flash[:message] = "Successfully created song."
+		redirect to("/songs/#{@song.slug}")
+	end
+
+	get '/songs/:slug/edit' do
+		@song = Song.find_by_slug(params[:slug])
+		erb :'/songs/edit'
+	end
+
+	post '/songs/:slug' do
+		@song = Song.find_by_slug(params[:slug])
+		@song.update(params[:song])
+		@artist = Artist.find_or_create_by(params[:artist])
+		@song.artist = @artist
+
+		params[:genres].each do |genre_id|
+			if !@song.genres.include?(Genre.all.find{|i| i.id == genre_id.to_i })
+					@song.genres << Genre.all.find{|i| i.id == genre_id.to_i }
+			end
+		end
+
+		@song.save
+		flash[:message] = "Successfully updated song."
+		redirect to("/songs/#{@song.slug}")
+	end
+
+end
